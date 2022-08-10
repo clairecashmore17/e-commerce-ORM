493f5118cef2450093a6b4c6457ea3aec8b095ef
clairecashmore17 <cbear5@live.com>
2022-08-10 16:38:50 -0700

    fixed DB dotenv bug

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, 'root', 'password', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USR, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
